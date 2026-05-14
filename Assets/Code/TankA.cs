06399e15ed6dc776c88014f58ff64e2fb6ecbe3f
Kapnack <unknowrealperson@gmail.com>
2026-05-14 20:08:52 -0300

    Fixes

--- Assets/Code/TankA.cs
+++ Assets/Code/TankA.cs
@@ -6,6 +6,13 @@ namespace Assets.Code
     {
         public override void CheckInput(float deltaTime)
         {
+            if (Input.GetKey(KeyCode.UpArrow))
+                canionForce += deltaTime;
+            else if (Input.GetKey(KeyCode.DownArrow))
+                canionForce -= deltaTime;
+
+            canionForce = Mathf.Max(canionForce, 0.0f);
+
             if (Input.GetKey(KeyCode.LeftArrow))
                 velocity += Vector2.left * (acceleration * deltaTime);
             else if (Input.GetKey(KeyCode.RightArrow))
@@ -16,6 +23,9 @@ namespace Assets.Code
             else if (Input.GetKey(KeyCode.O))
                 cannonRotation -= acceleration * 2f * deltaTime;
 
+            if (Input.GetKeyDown(KeyCode.Space))
+                shoot = true;
+
             cannonRotation = Mathf.Clamp(cannonRotation, 0f, 180f);
         }
     }
@@ -24,6 +34,13 @@ namespace Assets.Code
     {
         public override void CheckInput(float deltaTime)
         {
+            if (Input.GetKey(KeyCode.W))
+                canionForce += deltaTime;
+            else if (Input.GetKey(KeyCode.S))
+                canionForce -= deltaTime;
+
+            canionForce = Mathf.Max(canionForce, 0.0f);
+
             if (Input.GetKey(KeyCode.A))
                 velocity += Vector2.left * (acceleration * deltaTime);
             else if (Input.GetKey(KeyCode.D))
@@ -33,6 +50,9 @@ namespace Assets.Code
                 cannonRotation += acceleration * 2f * deltaTime;
             else if (Input.GetKey(KeyCode.E))
                 cannonRotation -= acceleration * 2f * deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.F))
+                shoot = true;
 
             cannonRotation = Mathf.Clamp(cannonRotation, 0f, 180f);
         }
